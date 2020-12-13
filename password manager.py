92341739c0165bc1d4ba9fb44b26dbd13b8fce20
Sharkrider120 <55744475+Sharkrider120@users.noreply.github.com>
2020-12-12 23:55:38 -0800

    Beta password manager

--- password manager.py
+++ password manager.py
@@ -1,14 +1,34 @@
-answer = input("Do you want to open or create a password?:\n")
+Featurelink = ('https://tinyurl.com/y3hex46c')
+Buglink = ('https://tinyurl.com/yy4o4rgc')
+version = ('1.0 beta')
+from time import sleep
+while True:
 
-if answer =="open":
-    code = input("Please enter the name of your password:\n")
-    f = open(code, "r")
-    print(f.read())
+	session = input("Do you want to add a password type add or type open to open a password or type help :\n")
 
-elif answer == "create":
-    global password
-    name = input("Please enter a name for your password:\n")
-    f = open(name, "x")
-    password = input("Please enter the password:\n")
-#    encrypt_message(password)
 
+
+	if session =="help":
+		help_com = input("type About, Bug, or Feature:\n")
+
+		if help_com ==("Feature"):
+			print("Click this link to suggest a new feature" + Featurelink )
+
+		if help_com ==("Bug"):
+			print("Please go to this link to report a bug " + Buglink )
+
+		if help_com ==("About"):
+			print("PASSWORD MANAGER By Cody Wagner v" + version )
+
+	if session =="open":
+		pass_open = input("What is the name of the password?:\n")
+		f = open(pass_open, "r")
+		print(f.read())
+		sleep(10)
+
+	if session =="add":
+		new_name = input("What is the name of your password?:\n")
+		f = open(new_name, "x")
+		password = input("Please enter the password:\n")
+		f = open(new_name, "w")
+		f.write(password)
